c1167bf693b2525e74c3d2ef70382011873b14a2
Mg Mg <mgmg164907@gmail.com>
2023-04-03 05:01:22 +0630

    add some style

--- cash/income/income.php
+++ cash/income/income.php
@@ -1,22 +1,22 @@
-
 <?php
 require("../database.php");
-$currentDate=$currentDate = date('Y-m-d');
-if(!isset($_SESSION['user']['id'])){
+$currentDate = $currentDate = date('Y-m-d');
+if (!isset($_SESSION['user']['id'])) {
   header("location:/login");
   exit;
-}else{
-$query=$conn->prepare("SELECT * FROM  category  where category_status=1");
+} else {
+  $query = $conn->prepare("SELECT * FROM  category  where category_status=1");
 
-$query->execute();
-$result = $query->get_result();
+  $query->execute();
+  $result = $query->get_result();
 
-require("../navbar.php");
+  require("../header.php");
+  require("../navbar.php");
 }
 
 ?>
 
-  <section class="vh-100" style="background-color: #508bfc;">
+<section class="vh-100 pt-5 bg-primary">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -24,42 +24,39 @@ require("../navbar.php");
           <div class="card-body p-5 text-center">
 
             <form action="income/create" method="post">
-            <h3 class="mb-5">Income</h3>
-            <div class="text-start mb-4">
-            <label for="date">Date :</label>
-            <input type="date" id="date" name="date" class="ms-1" required>
-          </div>
-            
-            <div class="form-outline mb-4">
-              <input type="number" name="amount" id="amount" class="form-control form-control-lg" />
-              <label class="form-label" for="amount">Amount</label>
-            </div>
-            <div class="text-start mb-4">
-            <label for="cars">Choose a category:</label>
+              <h3 class="mb-5">Income</h3>
+              <div class="text-start mb-4">
+                <label for="date">Date :</label>
+                <input type="date" id="date" name="date" class="ms-1" required>
+              </div>
 
-            <select name="category" id="category" class="ms-1">
-            <?php while($rows=mysqli_fetch_assoc($result)):?>
-              <option value="<?= $rows['cid'] ?>"><?=$rows['category_name']?></option>
-              <?php endwhile;?>
-            </select>
+              <div class="form-outline mb-4">
+                <input type="number" name="amount" id="amount" class="form-control form-control-lg" />
+                <label class="form-label" for="amount">Amount</label>
+              </div>
+              <div class="text-start mb-4">
+                <label for="cars">Choose a category:</label>
 
-            </div>
-          
-            <div class="form-outline mb-4">
-              <input type="text" name="description" id="description" class="form-control form-control-lg" />
-              <label class="form-label" for="description">Description</label>
-            </div>
+                <select name="category" id="category" class="ms-1">
+                  <?php while ($rows = mysqli_fetch_assoc($result)) : ?>
+                    <option value="<?= $rows['cid'] ?>"><?= $rows['category_name'] ?></option>
+                  <?php endwhile; ?>
+                </select>
 
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Income</button>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="text" name="description" id="description" class="form-control form-control-lg" />
+                <label class="form-label" for="description">Description</label>
+              </div>
+
+              <button class="btn btn-primary btn-lg btn-block" type="submit">Income</button>
             </form>
- 
+
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"
-></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
--- function.php
+++ function.php
@@ -1,10 +1,11 @@
 <?php
-function base_path($file){
+function base_path($file)
+{
   // var_dump(BASE_PATH);
   return BASE_PATH . $file;
 }
 
-function login($user){
-$_SESSION['user']=$user;
-
+function login($user)
+{
+  $_SESSION['user'] = $user;
 }
